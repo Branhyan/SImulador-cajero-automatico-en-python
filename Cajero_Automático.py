0afde2f1aa50a91cce95a50961841a6e4766f080
Branhyan <branhybernasp2@gmail.com>
2025-06-02 22:57:25 -0300

    agregué match-case, opciones en depositar y retiro, y historial básico

--- Cajero_Automático.py
+++ Cajero_Automático.py
@@ -1,15 +1,20 @@
+from datetime import datetime #Importo datetime para usar funciones de fecha y hora
+
 cuenta = {                                                        #Hago un diccionario con la cuenta enlazando el pin y cual es, así como el saldo y cual es
-    "pin": "1234",
+    "pin": 1234,
     "saldo": 1000
 }
 
+historial_transacciones = []                                      #Lista para guardar el historial de transacciones
+
 intentos_fallidos = 0                                      #Variable para contar los intentos fallidos al verificar el PIN
 contador_nuevo_pin = 0                                     #Variable para contarl las veces que se intenta cambiar al mismo PIN
+
 def verificar_pin():
     global intentos_fallidos                                       #global es para que use la variable que está definida fuera de la función, sino se haría una variable nueva dentro de la función
     while intentos_fallidos < 3:
         try:
-            pin_ingresado = input("Ingrese su PIN: ")               #Se pide el PIN al usuario, se usa int() para que sea un número y no un string, ya que el PIN es numérico
+            pin_ingresado = int(input("Ingrese su PIN: "))              #Se pide el PIN al usuario, se usa int() para que sea un número y no un string, ya que el PIN es numérico
             if pin_ingresado == cuenta["pin"]:
                 return True                                          #Si el PIN es correcto la función da True
             else:
@@ -23,25 +28,128 @@ def verificar_pin():
 def consultar_saldo():
     print(f"El saldo actual es de: ${cuenta["saldo"]}")             #Se usa diccionario[valor] para llamar a un valor específico de un diccionario
 
-def depositar_dinero():
-    monto = float(input("¿Cuanto dinero pensás depositar?:  "))
-    if monto < 0:
+def añadir_dinero(cantidad):                       #Hice una función para añadir dinero a la cuenta, para mayor limpieza del código
+    if cantidad < 0:
         print("Debes depositar una cantidad positiva.")
         print("Vuelve a intentarlo.")
     else:
-        cuenta["saldo"] += monto                                                   #+= se usa para sumar y a la vez modificar la variable con la suma
-        print(f"${monto} depositados. Ahora el saldo es de: ${cuenta['saldo']}")
+        cuenta["saldo"] += cantidad                                                  #+= se usa para sumar y a la vez modificar la variable con la suma
+        print(f"${cantidad} depositados. Ahora el saldo es de: ${cuenta['saldo']}")
+        historial_transacciones.append({"fecha": datetime.now().strftime("%d-%m-%Y %H:%M:%S"),"hora": datetime.now().strftime("%H:%M"), "tipo": "Depósito", "cantidad": cantidad})  #Guardo la transacción en el historial
+        return
+
+def quitar_dinero(cantidad):
+    if cantidad > cuenta["saldo"]:
+        print("No tienes suficiente saldo para retirar esa cantidad.")
+        return
+    if cantidad < 0: 
+        print("Debes retirar una cantidad positiva.")
+        print("Vuelve a intentarlo.")
+        return
+    else:
+        cuenta["saldo"] -= cantidad
+        print(f"Se han retirado ${cantidad} del saldo. Le quedan: ${cuenta['saldo']}")
+        historial_transacciones.append({"fecha": datetime.now().strftime("%d-%m-%Y %H:%M:%S"),"hora": datetime.now().strftime("%H:%M"), "tipo": "Retiro", "cantidad": cantidad})
+        return
+
+def ver_historial():
+    print("\n--- Historial de Transacciones ---")
+    for historial in historial_transacciones:
+        print(f"Fecha: {historial['fecha']}, Hora: {historial['hora']}, Tipo: {historial['tipo']}, Cantidad: ${historial['cantidad']}")
+
+def depositar_dinero():
+    try:
+        while True:
+                print("""
+            --- Depositar Dinero ---
+            1- 100
+            2-250
+            3-500
+            4-1000
+            5-1500
+            6- Ingresar cantidad
+            7- Volver al menú principal""")
+                
+                eleccion = int(input("Elija una opcion: "))
+                match eleccion: #Usé match para hacer un menú de opciones, es una forma más limpia de hacerlo con un if-elif-else
+                        case 1:
+                            añadir_dinero(100)
+                            break
+                        case 2:
+                            añadir_dinero(250)
+                            break
+                        case 3:
+                            añadir_dinero(500)
+                            break
+                        case 4:
+                            añadir_dinero(1000)
+                            break
+                        case 5:
+                            añadir_dinero(1500)
+                            break
+                        case 6:
+                            try:
+                                cantidad = float(input("Ingrese la cantidad a depositar: "))
+                                añadir_dinero(cantidad)
+                                break
+                            except ValueError:
+                                print("Debe ingresar un número. Vielve a intentar")
+                                continue
+                        case 7:
+                            print("Volviendo al menú principal...")
+                            return
+                        case _:
+                            print("Opción no válida, vuelve a intentarlo.")
+                            continue
+    except ValueError:
+        print("Debe ingresar un número. Intentelo nuevamente")
 
 def retirar_dinero():
     try:
-        cantidad = float(input("¿Cuanto dinero vas a retirar?:  "))
-        if cantidad > cuenta["saldo"]:
-            print("Saldo insuficiente ponga una cantidad adecuada")
-        else:
-            cuenta["saldo"] -= cantidad
-            print(f"Has retirado ${cantidad} ahora te quedan ${cuenta['saldo']}")
+        while True:
+                print("""
+            --- Retirar Dinero ---
+            1- 100
+            2-250
+            3-500
+            4-1000
+            5-1500
+            6- Ingresar cantidad
+            7- Volver al menú principal""")
+                
+                eleccion = int(input("Elija una opción: "))
+                match eleccion:
+                        case 1:
+                            quitar_dinero(100)
+                            break
+                        case 2:
+                            quitar_dinero(250)
+                            break
+                        case 3:
+                            quitar_dinero(500)
+                            break
+                        case 4:
+                            quitar_dinero(1000)
+                            break
+                        case 5:
+                            quitar_dinero(1500)
+                            break
+                        case 6:
+                            try:
+                                cantidad = float(input("Ingrese la cantidad a retirar: "))
+                                retirar_dinero(cantidad)
+                                break
+                            except ValueError:  
+                                print("Debes ingresar un número. Vuelve a intentar")
+                                continue
+                        case 7:
+                            print("Volviendo al menú principal...")
+                            return
+                        case _:
+                            print("Opción no válida, vuelve a intentarlo.")
+                            continue
     except ValueError:
-        print("Debe ingresar un número. Intentelo nuevamente") #Si no se ingresa un número en lugar de dar error, se le pide al usuario que ingrese un número
+        print("Debe ingresar un número. Intentelo nuevamente")
 def cambiar_pin():
     try:    
         if verificar_pin():
@@ -51,8 +159,11 @@ def cambiar_pin():
                 if len(nuevo_pin) != 4:                                  #Verifico que el nuevo PIN tenga 4 dígitos
                     print("El PIN debe de tener 4 dígitos. Vuelve a intentarlo.")
                     continue
-                if not nuevo_pin.isdigit():                                 #Verifico que el nuevo PIN sea sea un número
+                if not nuevo_pin.isdigit():                                 
                     print("El PIN solo debe tener numeros. Vuelve a intentarlo.")
+                    continue
+                if not nuevo_pin:
+                    print("Escribe un PIN, vuelva a intentarlo.")
                     continue
                 if nuevo_pin == cuenta["pin"]:                           #Verifico que el nuevo PIN no sea igual al actual
                     print("Es el mismo PIN que el actual, ingresa uno diferente.")
@@ -77,22 +188,25 @@ def menu():
             print("2. Depositar ")
             print("3. Retirar ")
             print("4. Cambiar PIN")
-            print("5. Salir")
-            opcion = input("Seleccione una opción: ")
-
-            if opcion == "1":
-                consultar_saldo()
-            elif opcion == "2":
-                depositar_dinero()
-            elif opcion == "3":
-                retirar_dinero()
-            elif opcion == "4":
-                cambiar_pin()
-            elif opcion == "5":
-                print("Gracias por usar el cajero, nos vemos!")
-                break
-            else:
-                print("Debes de elegir una de las opciones dadas, intenta de nuevo.")
+            print("5. Ver historial de transacciones")
+            print("6. Salir")
+            opcion = int(input("Seleccione una opción: "))
+            match opcion:
+                case 1:
+                    consultar_saldo()
+                case 2:
+                    depositar_dinero()
+                case 3:
+                    retirar_dinero()
+                case 4:
+                    cambiar_pin()
+                case 5:
+                    ver_historial()
+                case 6:
+                    print("Gracias por usar el cajero, nos vemos!")
+                    break
+                case _:
+                    print("Debes de elegir una de las opciones dadas, intenta de nuevo.")
     except ValueError:
         print("Debe ingresar un número. Intentelo nuevamente.")
 
